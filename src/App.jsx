b593f696015b87faed5a60698a7ef1da5c731b72
cozyGarage <e1500953@edu.vamk.fi>
2023-06-04 00:30:52 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <OthelloBoard/>
+    );
+  }
+}
+
+export default App;
