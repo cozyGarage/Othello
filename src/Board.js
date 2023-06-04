9191d947218a99b3ca1f0c5fe900788c6230a505
cozyGarage <e1500953@edu.vamk.fi>
2023-06-04 03:49:37 +0300

    Update

--- src/Board.js
+++ src/Board.js
@@ -12,21 +12,21 @@ const Board = ({board, onPlayerTurn}) => {
   }
 
   return (
-    <div className="Board">
+    <div className="Board shadow border">
       <div className="gameInfo">
-        <span className="playerInfo">
+        <span className="playerInfo shadow border">
           <span className="label"></span>
           <span className={player.toLowerCase()}>{player}</span>
         </span>
         <span className="spacer"/>
-        <span className="scoreInfo">
+        <span className="scoreInfo shawdow border">
           <span className="label"></span>
           <span className="black">{playerScore.black}</span>
           <span className="scoreDelimitter">-</span>
           <span className="white">{playerScore.white}</span>
         </span>
       </div>
-      <div className="board">
+      <div className="board shadow border">
         {rows}
       </div>
     </div>
--- src/Tile.js
+++ src/Tile.js
@@ -9,11 +9,11 @@ const Tile = ({tile, x, y, onPlayerTurn}) => {
     : x => x;
 
   return (
-    <span className={'Tile ' + tile} onClick={handleClick}>
+    <div className={'Tile ' + tile} onClick={handleClick}>
       <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
         <circle cx="100" cy="100" r="92"/>
       </svg>
-    </span>
+    </div>
   );
 }
 
